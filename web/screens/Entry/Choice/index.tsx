efa5be745a5ed9e75a1acef05109b43391e236db
Quentin Macheda <quentin.macheda@outlook.fr>
2024-05-07 16:41:16 +0100

    Update icons

---
+++ web/screens/Entry/Choice/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+const EntryChoiceScreen = () => {
+  return (
+    <div className="flex h-full w-full">
+      <div className="w-full">
+        <button>Private Mode</button>
+        <button>Online Mode</button>
+      </div>
+    </div>
+  )
+}
+
+export default EntryChoiceScreen
