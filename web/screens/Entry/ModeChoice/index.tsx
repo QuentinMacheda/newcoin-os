3cbece16c3e055d01702cb82c68ad9de32ea70d8
Quentin Macheda <quentin.macheda@outlook.fr>
2024-05-13 02:11:20 +0100

    Add style to Mode Choice page

--- web/screens/Entry/ModeChoice/index.tsx
+++ web/screens/Entry/ModeChoice/index.tsx
@@ -25,20 +25,21 @@ const EntryChoiceScreen = () => {
   const onClickMode = useLoadURL()
 
   return (
-    <div className="flex h-screen w-screen flex-1 overflow-hidden">
-      <div className="w-full">
-        <Link href={choicesList[0][0]}>
+    <main className="relative min-h-screen">
+      <div className="flex h-screen w-screen gap-x-6 p-6">
+        <Link href={choicesList[0][0]} className="block h-full w-full">
           <ChoiceItem name={choicesList[0][1]} text={choicesList[0][2]} />
         </Link>
         <button
           onClick={() => {
             onClickMode(choicesList[1][0])
           }}
+          className="block h-full w-full"
         >
           <ChoiceItem name={choicesList[1][1]} text={choicesList[1][2]} />
         </button>
       </div>
-    </div>
+    </main>
   )
 }
 
